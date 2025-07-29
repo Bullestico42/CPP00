cb9f92aba0709cf3df4c10d8035daff19aa68bc3
Alessio Piscopo <bullestico@MacBook-Air-de-Alessio.local>
2025-07-29 17:57:36 +0200

    CPP is good

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bullestico <bullestico@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 17:52:12 by bullestico        #+#    #+#             */
+/*   Updated: 2025/07/29 17:52:14 by bullestico       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if (ac < 2)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for (size_t i = 0; av[i]; i++)
+        for (size_t j = 0; av[i][j]; j++)
+            av[i][j] = toupper(av[i][j]);
+    for (size_t i = 1; av[i]; i++)
+        if ((int)i >= ac - 1) std::cout << av[i];
+            else
+                std::cout << av[i] << " ";
+    return 0;
+}
