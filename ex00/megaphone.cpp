c581d6735a71594a0dde6ef57e5dc998b8b72fab
Alessio Piscopo <bullestico@MacBook-Air-de-Alessio.local>
2025-10-01 17:47:51 +0200

    finito pipo

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bullestico <bullestico@student.42.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/29 17:52:12 by bullestico        #+#    #+#             */
-/*   Updated: 2025/08/21 18:06:36 by bullestico       ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <iostream>
 
 int main(int ac, char **av)
@@ -22,7 +10,7 @@ int main(int ac, char **av)
     for (size_t i = 1; av[i]; i++)
         if ((int)i >= ac - 1) std::cout << av[i];
             else
-                std::cout << av[i] << " ";
+                std::cout << av[i];
     std::cout << std::endl;
     return 0;
 }
